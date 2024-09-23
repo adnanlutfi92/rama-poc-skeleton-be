925bcf75dcd7e382b70391cf05f0d168b3ea819a
Rama Sullivan <ramasullivan27@gmail.com>
2024-09-23 22:25:52 +0700

    feat: create api handler

--- src/modules/testing/testing.controller.spec.ts
+++ src/modules/testing/testing.controller.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TestingController } from './testing.controller';
 import { TestingService } from './testing.service';
+import { ExternalModule } from '../external/external.module';
 
 describe('TestingController', () => {
   let testingController: TestingController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [ExternalModule],
       controllers: [TestingController],
       providers: [TestingService],
     }).compile();
