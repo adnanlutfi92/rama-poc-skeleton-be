8d150575528ae38a45fea1301271edfc9064a8fa
Rama Sullivan <ramasullivan27@gmail.com>
2024-09-24 16:46:13 +0700

    fix: linter and add pre push

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,7 +26,6 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'error',
     '@typescript-eslint/no-explicit-any': 'error',
     '@typescript-eslint/no-unused-vars': ['error', { args: 'none' }],
-    // 'no-console': 'warn', // for FE
     'no-undef': 'error',
     'no-restricted-syntax': [
       'error',
