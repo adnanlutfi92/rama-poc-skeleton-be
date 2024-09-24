89fb235d02a8e270c8a5c4d2034cb0bbc2402200
Rama Sullivan <ramasullivan27@gmail.com>
2024-09-24 09:32:25 +0700

    refactor: rename folder

--- src/modules/handler/external.module.ts
+++ src/modules/handler/external.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ApiService } from './api.service';
-import { DummyJsonService } from './DummyJson/dummy.service';
+import { DummyJsonService } from './dummy-json/dummy.service';
 
 @Module({
   imports: [],
